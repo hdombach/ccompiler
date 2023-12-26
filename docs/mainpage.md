060a9475f1d48b7efe32c3a05d3550e6d4929a5c
Hezekiah Dombach <hezekiahdombach@gmail.com>
2023-12-26 10:33:10 -0500

    added argParse documentation

--- docs/mainpage.md
+++ docs/mainpage.md
@@ -1,4 +1,14 @@
 @mainpage
 
-# Main page
-Hopefull this will work
+# CCompiler
+This is my attempt at a c compiler
+
+# Structure
+The program is split into multiple stages
+1. tokenizer
+2. preprocessor
+3. abstract syntax tree generation
+4. Semanting analysis
+5. *UNIMPLIMENTED*
+
+All of the stages can be seen in `main.c`
--- src/argParser.h
+++ src/argParser.h
@@ -2,19 +2,56 @@
 
 #include "util/wordList.h"
 
+/**
+ * @file
+ *
+ * Provides functions to parse ccompiler args and load the result into Args
+ */
+
+/**
+ * @brief parseArgs result
+ *
+ * Contains all the information contained in the cmd-lines args but in a
+ * friendly format
+ * Must be initialized with @ref initArgs before using
+ */
 typedef struct {
+	/** @brief List of files to compile */
 	WordList files;
+	/** @brief Whether the help message should be shown */
 	int help;
+	/** @brief The verbosity level */
 	int verbose;
 } Args;
 
+/** @brief A global instance of the parsed args */
 extern Args g_args;
 
+/** @brief The help message to pring */
 extern const char *ARGS_HELP_MSG;
 
+/** 
+ * @param[out] args
+ */
 void initArgs(Args *args);
+/**
+ * @param[in] args
+ */
 void freeArgs(Args *args);
 
+/**
+ * @brief Parse args for ccompiler
+ * @param[in] argc Arg count
+ * @param[in] argv List of args
+ * @param[out] args
+ *
+ * Parses the args passed directly by main.  
+ * Assumes that the first argv element is the name of the program
+ */
 int parseArgs(int argc, char **argv, Args *args);
 
+/**
+ * @brief Debug print args
+ * @param[in] args
+ */
 int argsPrint(Args const *args);
