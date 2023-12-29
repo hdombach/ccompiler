ef1dc2d1ec74b5af9ca8b5625d98201b364430ba
Hezekiah Dombach <hezekiahdombach@gmail.com>
2023-12-28 20:49:57 -0500

    added decl test stuff

--- src/tests/astTest.h
+++ src/tests/astTest.h
@@ -23,9 +23,6 @@ void astTestFile() {
 	tAstSuccess(
 			"int;",
 			(ASTNodeType[]) {AST_FILE, AST_DECLARATION, AST_TYPE_SPEC, AST_UNKNOWN});
-	tAstDebug(
-			"int;\n"
-			"char c;");
 
 	tAstSuccess(
 			"int;\n"
@@ -38,6 +35,85 @@ void astTestFile() {
 			});
 }
 
+void astDecl() {
+	tStartSection("Ast Declaration");
+
+	tAstSuccess(
+			"unsigned int numeric;\n"
+			"const unsigned int const_numberic;",
+			(ASTNodeType[]) {
+				AST_FILE,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_UNKNOWN
+			});
+
+	tAstSuccess(
+			"unsigned int numeric;\n"
+			"const unsigned int const_numberic;",
+			(ASTNodeType[]) {
+				AST_FILE,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_UNKNOWN
+			});
+
+	tAstSuccess(
+			"typedef long int size_t;\n"
+			"size_t typedef_value;",
+			(ASTNodeType[]) {
+				AST_FILE,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_IDENTIFIER_TS, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_UNKNOWN
+			});
+
+	tAstSuccess(
+			"struct struct_t struct_value;\n"
+			"struct {int m1; float m2;};\n"
+			"struct struct2_t {int m3;};\n",
+			(ASTNodeType[]) {
+				AST_FILE,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_STRUCT_DECL, AST_DECLARATOR, AST_IDENTIFIER_DECL, 
+				AST_DECLARATION, AST_TYPE_SPEC, AST_STRUCT_DECL,
+					AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+					AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_STRUCT_DECL,
+					AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_UNKNOWN
+			});
+
+	tAstSuccess(
+			"union union_t union_value;\n"
+			"union {int m1; float m2;};\n"
+			"union union2_t {int m3; };",
+			(ASTNodeType[]) {
+				AST_FILE,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_STRUCT_DECL, AST_DECLARATOR, AST_IDENTIFIER_DECL, 
+				AST_DECLARATION, AST_TYPE_SPEC, AST_STRUCT_DECL,
+					AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+					AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_STRUCT_DECL,
+					AST_DECLARATION, AST_TYPE_SPEC, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_UNKNOWN
+			});
+
+	tAstSuccess(
+			"enum enum_t enum_value;\n"
+			"enum enum2_t { VALUE_1, VALUE_2 = 2, VALUE_3 };\n"
+			"enum {VALUE_1 = 0, VALUE_2};\n",
+			(ASTNodeType[]) {
+				AST_FILE,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_ENUM_DECL, AST_DECLARATOR, AST_IDENTIFIER_DECL,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_ENUM_DECL,
+					AST_ENUM_CONST, AST_ENUM_CONST, AST_ENUM_CONST,
+				AST_DECLARATION, AST_TYPE_SPEC, AST_ENUM_DECL,
+					AST_ENUM_CONST, AST_ENUM_CONST, AST_ENUM_CONST,
+				AST_UNKNOWN
+			});
+}
+
 void astTests() {
 	astTestFile();
+	astDecl();
 }
